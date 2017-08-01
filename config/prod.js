c9abea133d228d45dcd650eaeb08a2a9ce14439e
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-08-01 21:55:08 +0000

    deploying application

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,7 @@
 module.exports = {
-  steamAPI: "BB7503139E94E830321E674D28DB18DE",
-  mongoURI:
-    "mongodb://philliprognerud35:deskjet700phil@ds127783.mlab.com:27783/csgo-match-prod",
-  cookieKey: "TRDunioRTYiioopmbtfrhkjjhghg",
-  returnURL: "http://54.186.130.14/auth/steam/return",
-  realm: "http://54.186.130.14/"
+  steamAPI: process.env.STEAM_KEY,
+  mongoURI: process.env.MONGO,
+  cookieKey: process.env.COOKIE,
+  returnURL: process.env.RETURN,
+  realm: process.env.REALM
 };
