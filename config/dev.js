c5900996479152e87eaf160ffcf93c0d945eb049
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-08-12 20:39:04 +0000

    removed cached folders from git

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  steamAPI: "BB7503139E94E830321E674D28DB18DE",
-  mongoURI: "mongodb://phillip:deskjet700@ds127963.mlab.com:27963/csgo-11193",
-  cookieKey: "agnbg328DFighwg84dk",
-  returnURL: "https://csgo-match-whatthephil.c9users.io/auth/steam/return",
-  realm: "https://csgo-match-whatthephil.c9users.io/",
-  steamUser: "whatthephill",
-  steamPw: "deskjet700"
-};
--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  steamAPI: process.env.STEAM_KEY,
-  mongoURI: process.env.MONGO,
-  cookieKey: process.env.COOKIE,
-  returnURL: process.env.RETURN,
-  realm: process.env.REALM,
-  steamUser: process.env.STEAM_USER,
-  steamPw: process.env.STEAM_PW
-};
