a7e82cc54680cf18afc4d798f333eeccb8708cc2
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-08-13 18:45:17 +0000

    starting post route to backend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,3 +10,4 @@ export const fetchUser = () => async dispatch => {
 // export const searchUser = async dispatch => {
 //   const res = await axios.get("")
 // }
+//https://stackoverflow.com/questions/43793637/post-form-data-to-server-using-react-js-and-express-js
